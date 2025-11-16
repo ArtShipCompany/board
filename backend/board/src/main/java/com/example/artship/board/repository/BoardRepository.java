88a4af2f0ca22b243a3b4d7f9923586e9ce279d2
sinveria <perevalovav0108@gmail.com>
2025-11-16 15:14:43 +0300

    create board repository

---
+++ backend/board/src/main/java/com/example/artship/board/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.example.artship.board.repository;
+
+import com.example.artship.board.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
