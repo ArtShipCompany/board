16ea57fb5ba622bb61b0682d486725c4120339ff
sinveria <perevalovav0108@gmail.com>
2026-04-14 11:55:36 +0300

    added new model strokedata for ws

---
+++ backend/board/src/main/java/com/example/artship/board/model/ws/StrokeData.java
@@ -0,0 +1,44 @@
+package com.example.artship.board.model.ws;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StrokeData {
+    private String strokeId;
+    private String userId;
+    private String roomId;
+    private String type;
+
+    private List<Point> points;
+    
+    private String color;
+    private int size;
+    private double opacity;
+
+    private String shapeType;
+    
+    private String textContent;
+    private String fontFamily;
+    private int fontSize;
+    
+    private long timestamp;
+    
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Point {
+        private double x;
+        private double y;
+        private double pressure;
+        private long t;
+    }
+}
